10fcc3537804dad5fd23122b4e598e8075221fd7
Cannon431 <pokazanov14@gmail.com>
2018-07-31 14:18:04 +0600

    Auth fixes

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,9 +36,4 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
-
-    protected function getFailedLoginMessage()
-    {
-        return 'what you want here.';
-    }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,6 +45,7 @@
                         <!-- Authentication Links -->
                         @guest
                             <li><a href="{{ route('login') }}">Войти</a></li>
+                            <li><a href="{{ url('/') }}">На главную</a></li>
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true" v-pre>
